4ed3013cd7aff388d2c2f2c58b320ae26e5001ae
stickfig8 <gusrb0482@gmail.com>
2025-07-09 01:30:42 +0900

    fix: 메인 스크린 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import "./App.css";
 import MainFooter from "./components/MainFooter";
 import MainHeader from "./components/MainHeader";
-import PopularList from "./components/PopularList";
-import ThemeList from "./components/ThemeList";
-import AiRecList from "./components/AiRecList";
-import SearchPanel from "./components/SearchPannel";
+import MainScreen from "./screens/MainScreen";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <MainHeader />
-        <main className="flex flex-col gap-[72px] py-[72px]">
-          <SearchPanel />
-          <ThemeList />
-          <PopularList />
-          <AiRecList />
-        </main>
+        <MainScreen />
         <MainFooter />
       </div>
     </BrowserRouter>
--- src/screens/MainScreen.tsx
+++ src/screens/MainScreen.tsx
@@ -1,3 +1,15 @@
+import PopularList from "@/components/PopularList";
+import SearchPanel from "@/components/SearchPannel";
+import ThemeList from "@/components/ThemeList";
+import AiRecList from "@/components/AiRecList";
+
 export default function MainScreen() {
-  return <main></main>;
+  return (
+    <main className="flex flex-col gap-[72px] py-[72px]">
+      <SearchPanel />
+      <ThemeList />
+      <PopularList />
+      <AiRecList />
+    </main>
+  );
 }
