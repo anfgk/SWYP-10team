34661f41af813c1a49003dd65a32371e0c45ebd0
anfgk <gloria020510@gmail.com>
2025-07-09 00:13:27 +0900

    경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import MainFooter from './components/MainFooter';
+import MainHeader from './components/MainHeader';
+import PopularList from './components/PopularList';
+import ThemeList from './components/ThemeList';
+import AiRecList from './components/AiRecList';
+import SearchPanel from './components/SearchPannel';
+import MyReview from './screens/MyReview';
 
 function App() {
     return (
-      <>
-        <h1 className='text-xl'>어디가냥 같이가개</h1>
-      </>
-  )
+      <BrowserRouter>
+        <div className='App'>
+          <MainHeader />
+          <div className='max-w-[1200px] mx-auto px-4'>
+            <Routes>
+              <Route path="/" element={
+                <main className='flex flex-col gap-[72px] py-[72px]'>
+                  <SearchPanel />
+                  <ThemeList />
+                  <PopularList />
+                  <AiRecList />
+                </main>
+              } />
+              <Route path="/myreview" element={<MyReview />} />
+            </Routes>
+            <MainFooter />
+          </div>
+        </div>
+      </BrowserRouter>
+  );
 }
 
 export default App;
