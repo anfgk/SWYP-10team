fe59b861d71914d6803246af8d20fe5a6deb42ea
stickfig8 <gusrb0482@gmail.com>
2025-07-25 02:06:21 +0900

    chore: 로그인 콜백페이지 수정

--- src/pages/AuthCallBackPage.tsx
+++ src/pages/AuthCallBackPage.tsx
@@ -1,11 +1,16 @@
 import useIssueAccessToken from "@/hooks/useIssueAccessToken";
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
 const AuthCallBackPage = () => {
   useIssueAccessToken();
   const navigate = useNavigate();
-  const redirectURL = sessionStorage.getItem("loginLocation") || "/";
 
-  navigate(redirectURL);
+  useEffect(() => {
+    const redirectURL = sessionStorage.getItem("loginLocation") || "/";
+    navigate(redirectURL);
+  }, [navigate]);
+
   return <div>로그인 중...</div>;
 };
 
