98ece53a0b9227f7be2a4614e1ba1874d4c0d099
stickfig8 <gusrb0482@gmail.com>
2025-08-05 23:32:18 +0900

    chore: 사진 모달 활성화 스타일 변경

--- src/components/modals/photoSlideModal/PhotoCard.tsx
+++ src/components/modals/photoSlideModal/PhotoCard.tsx
@@ -9,10 +9,12 @@ interface Props {
 const ReviewPhotoCard = ({ img, isActive, onClick }: Props) => {
   return (
     <MainCard
-      className={`w-[116px] h-[116px] bg-cover cursor-pointer${isActive && " border-3 border-[var(--main-color)]"}`}
+      className={`relative w-[116px] h-[116px] bg-cover cursor-pointer overflow-hidden ${isActive && "ring-3 ring-inset ring-[var(--main-color)]"}`}
       style={{ backgroundImage: `url(${img})` }}
       onClick={onClick}
-    />
+    >
+      <div className="absolute inset-0 bg-black/0 hover:bg-black/20 transition-colors duration-100" />
+    </MainCard>
   );
 };
 
