be08bec3ebeac00f4b206c66d82c67e1b7e687f6
stickfig8 <gusrb0482@gmail.com>
2025-08-11 04:24:57 +0900

    fix: 빌드 오류 해결

--- src/components/auth/ProtectedMyPageLayout.tsx
+++
@@ -1,9 +0,0 @@
-import ProtectedRoute from "./ProtectedRoute";
-import MypageLayout from "../layouts/MypageLayout";
-const ProtectedLayout = (
-  <ProtectedRoute>
-    <MypageLayout />
-  </ProtectedRoute>
-);
-
-export default ProtectedLayout;
